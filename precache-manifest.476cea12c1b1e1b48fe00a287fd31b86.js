ec2cc63825b67ea17a4fbe283298431a28ff0206
keyberthRengel <keyrengel24@gmail.com>
2019-09-24 21:55:06 +0200

    inicio de la app

---
+++ precache-manifest.476cea12c1b1e1b48fe00a287fd31b86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9998e8bab92ffb72dd549d38e917a1",
+    "url": "/crud-react-node/index.html"
+  },
+  {
+    "revision": "59fd417b205869c70416",
+    "url": "/crud-react-node/static/css/main.8090ab59.chunk.css"
+  },
+  {
+    "revision": "02eeb403d9d68befae2b",
+    "url": "/crud-react-node/static/js/2.390e0045.chunk.js"
+  },
+  {
+    "revision": "59fd417b205869c70416",
+    "url": "/crud-react-node/static/js/main.7a138c3f.chunk.js"
+  },
+  {
+    "revision": "e2a28103a6faa3e90a5f",
+    "url": "/crud-react-node/static/js/runtime~main.f847d152.js"
+  }
+]);
